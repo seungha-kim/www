e1dc698ba7528b572ff4d9e58024964b8298bf8c
seulbinim <seulbinim@gmail.com>
2017-11-17 11:54:58 +0900

    메인메뉴 제어를 위한 jQuery 및 CSS 수정

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
   var menu = $('.main-menu > li');
-  var subMenu = $('.sub-menu');
-  var last = $('.sub-menu li:last-child a');
 
   menu.hover(function() {
     $(this).find('.sub-menu').toggleClass('menu-act');
   });
   menu.focusin(function() {
+    $(this).siblings().find('.sub-menu').removeClass('menu-act');
     $(this).find('.sub-menu').addClass('menu-act');
-  });
-  last.focusout(function() {
-    $(this).parents('.sub-menu').removeClass('menu-act');
   });
 });
