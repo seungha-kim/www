80798ba4fcd8b6cb5000695e9cc7343e89220cab
seulbinim <seulbinim@gmail.com>
2017-11-21 17:12:20 +0900

    검색 및 공지사항/자료실 마크업 및 디자인 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   var menu = $('.main-menu > li');
+  var tab = $('.notice-heading, .pds-heading')
 
+  // 메인메뉴
   menu.hover(function() {
     $(this).find('.sub-menu').toggleClass('menu-act');
   });
@@ -8,4 +10,10 @@ $(document).ready(function() {
     $(this).siblings().find('.sub-menu').removeClass('menu-act');
     $(this).find('.sub-menu').addClass('menu-act');
   });
+
+  // 탭메뉴
+  tab.on('click focusin', function() {
+    $(this).parent().addClass('board-act').siblings().removeClass('board-act');
+  });
+
 });
