c26c6cce8bb0cc3b22b4cccfa6cafc1338549932
seulbinim <seulbinim@gmail.com>
2017-11-23 16:36:49 +0900

    e.preventDefault(); 추가

--- js/tab-aria.js
+++ js/tab-aria.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('[role="tab"]').on('click', function() {
+  $('[role="tab"]').on('click', function(e) {
+    e.preventDefault();
     $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
     var selectedId = "#" + $(this).attr('aria-controls');
     $(selectedId).addClass('tabpanel-open').siblings().removeClass('tabpanel-open');
